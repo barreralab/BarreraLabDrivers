3dcb054950204a83f6459b824b1442cc4ff867a0
fishyvishy <vishy235@gmail.com>
2024-10-21 17:17:15 -0400

    acdac9106 tests

--- tests/test_acdac9106.py
+++ tests/test_acdac9106.py
@@ -1,44 +1,59 @@
-import qcodes as qc
+import unittest
 
+# import qcodes as qc
+# from qcodes.extensions import DriverTestCase
+import time
 from barreralabdrivers.drivers import ACDAC9106
-from time import sleep
-
-dac = ACDAC9106("acdac", "ASRL5::INSTR")
-
-station = qc.Station()
-
-dac.reset()
-
-station.add_component(dac)
 
 
-dac.ch3.voltage(400)
-dac.ch4.voltage(400)
+class TestDCDAC(unittest.TestCase):
+    def setUp(self):
+        self.acdac = ACDAC9106("dcdac", "ASRL6::INSTR")
+        self.acdac.reset()
+
+    def test_bad_values(self):
+        for chan_num in range(4):
+            self.assertRaises(ValueError, self._send_data, chan_num, -0.1)
+            self.assertRaises(ValueError, self._send_data, chan_num, 451)
+
+    def test_reset(self):
+        self.acdac.reset()
+        for chan in self.acdac.channels:
+            self.assertAlmostEqual(chan.voltage(), 0, 3)
+
+    # @unittest.skip("demonstrating skipping")
+    def test_longevity(self):
+        volts = []
+        start_time = time.time()
+        start = 0
+        end = 450
+        step = 10
+        for volt in range(start, end, step):
+            for chan in self.acdac.channels:
+                chan.voltage(volt)
+                meas = chan.voltage()
+                if abs(meas - volt) > 0.0005:
+                    print(f"Bad at {chan} with {volt}")
+                volts.append(chan.voltage())
+        end_time = time.time()
+
+        ops = (end - start) / step * 4
+        time_per_op = (end_time - start_time) / ops
+        print(
+            f"{end_time - start_time} seconds for {ops} operations -> {time_per_op}s/op"
+        )
+
+        self.assertLessEqual(time_per_op, 0.1)
+
+    def _send_data(self, chan: int, val: float):
+        self.acdac.channels[chan].voltage(val)
+
+    def _get_data(self, chan: int):
+        return self.acdac.channels[chan].voltage()
+
+    def tearDown(self) -> None:
+        self.acdac.close()
 
 
-dac.ch4.phase(24)
-
-# dac.display_mode("FOCUS4")
-
-dac.print_readable_snapshot()
-
-while True:
-    volts = float(input("Enter voltage: "))
-    if volts < 0:
-        break
-    try:
-        dac.ch4.voltage(volts)
-    except ValueError:
-        print("Invalid voltage")
-    except:
-        dac.error()
-
-    print(dac.error())
-
-for i in range(4):
-    print(dac.channels[i].phase())
-
-dac.frequency(24e3)
-
-
-dac.close()
+if __name__ == "__main__":
+    unittest.main()
