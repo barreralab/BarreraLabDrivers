2c8a0764dacf776618d0d6a086bb66909d6819a4
fishyvishy <vishy235@gmail.com>
2025-02-04 19:01:18 -0500

    dc box debugged

---
+++ Issues.md
@@ -0,0 +1,7 @@
+# Issues 
+
+1. serial-comm utility can cause issues when user exits communication. Threads may not be properly dealt with and spurious connections can persist, leading to connection issues on other interfaces 
+
+
+## Features to add 
+1. Documentation for custom drivers. Example notebooks and API. 
--- src/barreralabdrivers/utils/param_utils.py
+++ src/barreralabdrivers/utils/param_utils.py
@@ -2,15 +2,61 @@ import numpy as np
 from time import sleep
 
 
-def paramp(param, final=0, steps=40, sleep_time=0.05):
+def paramp(
+    params: tuple,
+    final: tuple = (0),
+    steps: int = 40,
+    sleep_time: float = 0.05,
+    track=True,
+):
     """
-    Smoothly transitions a parameter to a specified final value.
+    Smoothly transitions a group of parameters to a specified final value.
 
     Args:
-        param (callable): A callable that gets and sets the parameter value.
-        final (float): The target value to transition to.
+        param (tuple of callables): A callable that gets and sets the parameter value.
+        final (tuple of floats): The target value to transition to.
+        steps (int): the number of steps which the paramp will take
+        sleep_time (float): time per step (s)
+        track (bool): Choose whether the paramaters ramp at the same time or sequentially
+
     """
-    points = np.linspace(param(), final, steps)
-    for p in points:
-        param(p)
-        sleep(sleep_time)
+
+    # this is a legacy function which ensures that passing a single callable as params will work
+    if type(params) != tuple:
+        points = np.linspace(params(), final, steps)
+        for point in points:
+            params(point)
+            sleep(sleep_time)
+
+    # this section will handle tuples of params and final
+    if type(params) == tuple:
+        points = {}
+        # this loop defines the points for user defined endpoint
+        if final != (0):
+            if len(final) != len(params):
+                raise ValueError("final and param must be the same length")
+            for i in range(len(params)):
+                par = params[i]
+                points[i] = np.linspace(par(), final[i], steps)
+
+        # this loop defines points for ramping to zero
+        elif final == (0):
+            for i in range(len(params)):
+                par = params[i]
+                points[i] = np.linspace(par(), 0, steps)
+
+        # This ramps the parameters together
+        if track == True:
+            for step in range(steps):
+                for i in range(len(params)):
+                    par = params[i]
+                    par(points[i][step])
+                sleep(sleep_time)
+
+        # This ramps the parameters sequentially
+        elif track == False:
+            for i in range(len(params)):
+                for point in points[i]:
+                    par = params[i]
+                    par(point)
+                    sleep(sleep_time)
